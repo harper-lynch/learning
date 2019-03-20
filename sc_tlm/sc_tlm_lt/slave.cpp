b9156cb143f55d53fa8d708e99501248e3f3ab7d
harper-lynch <harper_lynch_is@icloud.com>
2019-03-20 21:24:56 +0800

    added process time in slave

--- sc_tlm/sc_tlm_lt/slave.cpp
+++ sc_tlm/sc_tlm_lt/slave.cpp
@@ -17,10 +17,12 @@ SC_MODULE(slave) {
     }
 
     virtual void b_transport(tlm::tlm_generic_payload &trans, sc_time & delay) {
+        sc_time process_time = sc_time(5, SC_NS);
         if ( trans.get_command() == tlm::TLM_WRITE_COMMAND ) {
             cout << "@" << sc_time_stamp() << " Write Command, address: 0x"<<hex<<trans.get_address()<<endl;
         }
-        wait(delay);
+        wait(process_time);
+        delay = delay + process_time;
         trans.set_response_status(tlm::TLM_OK_RESPONSE);
     }
 };
